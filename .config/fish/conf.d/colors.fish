cf447f74ca9ee26030e6940db41f841d848d0304
Bálint Hegyi <hegyi.balint.b@gmail.com>
2018-01-12 19:24:57 +0100

    Add i3 and updated fish configs

---
+++ .config/fish/conf.d/colors.fish
@@ -0,0 +1,19 @@
+set fish_color_normal          green --bold
+set fish_color_command         green --bold
+set fish_color_quote           yellow
+set fish_color_redirection     yellow
+set fish_color_end             yellow
+set fish_color_error           red --bold
+set fish_color_param           normal
+set fish_color_comment         green
+set fish_color_match           --background=yellow
+set fish_color_search_match    --background=white
+set fish_color_operator        normal
+set fish_color_escape          green
+set fish_color_autosuggestion  brgreen
+
+set fish_pager_color_prefix         normal --bold --underline
+set fish_pager_color_completion     green
+set fish_pager_color_description    normal      
+set fish_pager_color_progress       blue
+set fish_pager_color_secondary      normal
---
+++ .config/fish/conf.d/env.fish
@@ -0,0 +1,9 @@
+set DEV_HOME /Developer
+set DEV_SDK_HOME $DEV_HOME/SDK
+set DEV_CODE_HOME $DEV_HOME/Coding
+
+set PATH /usr/local/bin /usr/local/sbin $PATH 
+
+alias coding "cd $DEV_CODE_HOME"
+alias modes "cd $DEV_CODE_HOME/Misc/BME-MoDeS3"
+alias ingraph "cd $DEV_CODE_HOME/Java/ingraph"
